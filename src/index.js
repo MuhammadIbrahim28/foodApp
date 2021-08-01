258ca52e202bbeba3fb327027a4da21472d01bd7
MuhammadIbrahim28 <77695024+MuhammadIbrahim28@users.noreply.github.com>
2021-08-01 08:37:07 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDom from "react-dom";
+ReactDom.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Apple</li>
+      <li>Banana</li>
+      <li>Orange</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
